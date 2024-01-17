c5ad04cde8fd2369d37ea9cb2e9d7cf37e306b34
LongYinan <lynweklm@gmail.com>
2024-01-17 22:29:54 +0800

    Add github badge

--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <Toaster />
+    <a
+      href="https://github.com/napi-rs/node-rs"
+      target="_blank"
+      className="tw-fixed tw-top-0 tw-right-0 tw-m-4 tw-z-50 tw-fill-slate-50 tw-stroke-slate-200"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+        <path d="M9 18c-4.51 2-5-2-7-2" />
+      </svg>
+    </a>
   </React.StrictMode>
 )
